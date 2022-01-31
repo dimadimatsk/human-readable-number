645eb9033f22cab44cfe8c4553ffdbb5d81eb939
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2022-01-31 20:17:15 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let numToString = number.toString();
+
+    if (number === 0) return "zero";
+
+    if (number < 20) return ones[number];
+
+    if (numToString.length === 2) {
+        return (tens[numToString[0]] + " " + ones[numToString[1]]).trim();
+    }
+
+    if (numToString.length == 3) {
+        if (numToString[1] === "0" && numToString[2] === "0")
+            return ones[numToString[0]] + " hundred";
+        else
+            return (
+                ones[numToString[0]] +
+                " hundred " +
+                toReadable(+(numToString[1] + numToString[2]))
+            );
+    }
+};
